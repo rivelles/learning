06b78d49de988e77e243f6873daebaa4389f91f1
Lucas Rivelles <lucas.rivelles@glovoapp.com>
2023-11-20 23:07:12 +0100

    Small refactor

--- books/designing-data-intensive-applications/examples/lsm-tree/lsm-tree/src/main/kotlin/LSMTree.kt
+++ books/designing-data-intensive-applications/examples/lsm-tree/lsm-tree/src/main/kotlin/LSMTree.kt
@@ -71,21 +71,15 @@ class LSMTree(capacity: Int = 100) {
     class MemTable(private val capacity: Int = 100) {
         private val map = mutableMapOf<String, String>()
 
-        fun get(key: String): Any? {
-            return map[key]
-        }
+        fun get(key: String): Any? = map[key]
 
         fun put(key: String, value: String) {
             map[key] = value
         }
 
-        fun isFull(): Boolean {
-            return map.size >= capacity
-        }
+        fun isFull() = map.size >= capacity
 
-        fun clear() {
-            map.clear()
-        }
+        fun clear() = map.clear()
 
         fun createSegment(nextSegment: Long) = Segment(nextSegment.toString(), map)
     }
